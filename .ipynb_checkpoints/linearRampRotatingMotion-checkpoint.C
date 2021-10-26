72175bd07fa3431abe75e031dd5ab9c050a74aca
pnielsenMKL <pnielse@uwo.ca>
2021-10-26 10:50:16 -0300

    Set up code
    Reset variables, updated omega update to linear ramp to desired value, included if statement to ensure omega is fixed after ramp portion, set up make files

--- .ipynb_checkpoints/linearRampRotatingMotion-checkpoint.C
+++ .ipynb_checkpoints/linearRampRotatingMotion-checkpoint.C
@@ -22,7 +22,7 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 \*---------------------------------------------------------------------------*/
-s
+
 #include "linearRampRotatingMotion.H"
 #include "addToRunTimeSelectionTable.H"
 #include "mathematicalConstants.H"
@@ -58,17 +58,14 @@ Foam::solidBodyMotionFunctions::linearRampRotatingMotion::linearRampRotatingMoti
     solidBodyMotionFunction(SBMFCoeffs, runTime),
     mesh_(time_.db().parent().lookupObject<fvMesh>("region0")),
     coeffs_(SBMFCoeffs.subDict("linearRampRotatingMotionCoeffs")),
-    patchSet_(coeffs_.lookup("patches")),
-    pName_(coeffs_.lookupOrDefault<word>("pName", "p")),
-    rhoRef_(readScalar(coeffs_.lookup("rhoRef"))),
     origin_(coeffs_.lookup("origin")),
     axis_(coeffs_.lookup("axis")),
-    momentOfIntertia_(readScalar(coeffs_.lookup("momentOfIntertia"))),
-    opposingTorque_(readScalar(coeffs_.lookup("opposingTorque"))),
-    omega_(coeffs_.lookupOrDefault<scalar>("omega", 0)),
+    omegaIn_(coeffs_.lookupOrDefault<scalar>("omegaInitial", 0)),
+    omegaFin_(coeffs_.lookupOrDefault<scalar>("omegaFinal", 0)),
+    thetaIn_(coeffs_.lookupOrDefault<scalar>("thetaInitial", 0)),
+    thetaFin_(coeffs_.lookupOrDefault<scalar>("thetaFinal", 0)),
 //    angle_(0),
-    angle_(coeffs_.lookupOrDefault<scalar>("angle", 0)),//added 
-    alpha_(0)
+    angle_(coeffs_.lookupOrDefault<scalar>("angle", 0))//added 
 {}
 
 
@@ -83,105 +80,24 @@ Foam::solidBodyMotionFunctions::linearRampRotatingMotion::~linearRampRotatingMot
 void
 Foam::solidBodyMotionFunctions::linearRampRotatingMotion::updateAngle()
 {
-    // Typedefs
-    typedef incompressible::momentumTransportModel icoTurbModel;
-
-    // Lookup turbulence model
-    // - Note: this is currently limited to incompressible turbulence models
-    const incompressible::momentumTransportModel& turb =
-      mesh_.lookupObject<icoTurbModel>(momentumTransportModel::typeName);
-
-    // Initialize forces/moments
-    vector pressureForce = vector(0,0,0);
-    vector pressureMoment = vector(0,0,0);
-    vector viscousForce = vector(0,0,0);
-    vector viscousMoment = vector(0,0,0);
-    vector fluidForce = vector(0,0,0);
-    vector fluidMoment = vector(0,0,0);
-
-    // Get fields for calculating forces/moments
-    const volScalarField& p = mesh_.lookupObject<volScalarField>(pName_);
-    const volSymmTensorField& devSigma = turb.devSigma();
-
-    forAll(patchSet_, i)
-    {
-        // Get patch index
-        label patchI = mesh_.boundaryMesh().findPatchID(patchSet_[i]);
-
-        // Get displacement vectors for faces from origin
-        vectorField Md
-        (
-            mesh_.C().boundaryField()[patchI] - origin_
-        );
-
-        // Get normal forces on faces
-        vectorField Fp
-        (
-            rhoRef_*p.boundaryField()[patchI]*mesh_.boundary()[patchI].Sf()
-        );
-
-        // Get moment of pressure force on each face
-        vectorField Mp
-        (
-            Md ^ Fp
-        );
-
-        // Get viscous forces on faces
-        vectorField Fv
-        (
-            rhoRef_*devSigma.boundaryField()[patchI]&mesh_.boundary()[patchI].Sf()
-        );
-
-        // Get moment of viscous force on each face
-        vectorField Mv
-        (
-            Md ^ Fv
-        );
-
-        // Sum the pressure force/moment
-        pressureForce += sum(Fp);
-        pressureMoment += sum(Mp);
-        viscousForce += sum(Fv);
-        viscousMoment += sum(Mv);
-    }
-
-    // Combine for parallel case
-    Pstream::combineGather(pressureForce, plusEqOp<vector>());
-    Pstream::combineGather(viscousForce, plusEqOp<vector>());
-    Pstream::combineGather(pressureMoment, plusEqOp<vector>());
-    Pstream::combineGather(viscousMoment, plusEqOp<vector>());
-    Pstream::combineScatter(pressureForce);
-    Pstream::combineScatter(viscousForce);
-    Pstream::combineScatter(pressureMoment);
-    Pstream::combineScatter(viscousMoment);
-
-    fluidForce = pressureForce + viscousForce;
-    fluidMoment = pressureMoment + viscousMoment;
-
-    Info << "Pressure force " << pressureForce << endl;
-    Info << "Pressure moment " << pressureMoment << endl;
-    Info << "Viscous force " << viscousForce << endl;
-    Info << "Viscous moment " << viscousMoment << endl;
-    Info << "fluid force " << fluidForce << endl;
-    Info << "fluid moment " << fluidMoment << endl;
-
     //scalar t = time_.value();
 
     // Get timestep data
     scalar dt = time_.deltaTValue();
-    scalar dto = time_.deltaT0Value();
 
     // Update the motion
-    omega_ += 0.5*dto*alpha_;
     angle_ += dt*omega_;
-    scalar appliedMoment = fluidMoment & axis_;
-//    scalar opposingTorque = sign(appliedMoment)*opposingTorque_;
-    scalar opposingTorque = opposingTorque_*omega_;
-    alpha_ = (appliedMoment - opposingTorque)/momentOfIntertia_;
-    omega_ += 0.5*dt*alpha_;
+    
+    if (angle_ < thetaFin_)
+    {
+        omega_ = omegaIn_ - (omegaIn_ - omegaFin_)*(angle_/thetaFin_);        
+    }
+    else
+    {
+        omega_ = omegaFin_;
+    }
 
     Info << "omega " << omega_ << endl;
-    Info << "opposingTorque " << opposingTorque << endl;
     Info << "angle " << angle_ << endl;
 }
 
